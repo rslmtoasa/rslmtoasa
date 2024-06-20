12493a94b00ed953c459af8236a83a439bafecac
Anders Bergman <anders.bergman@physics.uu.se>
2024-06-20 12:46:33 +0200

    Added CTest functionality

--- tests/regression/bccFe_lanczos/oneliner.sh
+++ tests/regression/bccFe_lanczos/oneliner.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-rm -f Fe_out.nml ; ../../../build/bin/rslmto.x > testrun.log ; mv Fe_out.nml data.nml ; cp Fe.nml.ref ref.nml ;   pytest --tb=line  --no-header ../test_comparison.py ; rm -f data.nml ref.nml fort.* clust mad.mat map sbar str.out ves.out view.sbar
+
+if [ $# -eq 1 ]; then
+    binary="$1"
+else
+    binary=../../../build/bin/rslmto.x
+fi
+
+rm -f Fe_out.nml data.nml ref.nml fort.* clust mad.mat map sbar str.out ves.out view.sbar
+$binary > testrun.log 2>&1
+mv Fe_out.nml data.nml
+cp Fe.nml.ref ref.nml
+pytest --tb=line  --no-header ../test_comparison.py
+# rm -f data.nml ref.nml fort.* clust mad.mat map sbar str.out ves.out view.sbar
+if [ $? -ne 0 ]; then
+    echo "$0: ERROR: $binary failed"
+    exit 1
+fi
