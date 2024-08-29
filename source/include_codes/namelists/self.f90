e23faa19742de89108f34ffc1ee2328ec85bb2a6
Ramon Cardias Alves de Almeida <ramon_cardias@hotmail.com>
2024-08-29 20:53:36 +0200

    added namelists that wasnt added before

--- source/include_codes/namelists/self.f90
+++ source/include_codes/namelists/self.f90
@@ -1,10 +1,10 @@
-logical :: ws_all, rigid_band, orbital_polarization, mixmag_all, mix_all, magnetic_mixing, freeze, all_inequivalent
+logical :: ws_all, rigid_band, orbital_polarization, mixmag_all, mix_all, magnetic_mixing, freeze, all_inequivalent, fix_soc
 integer :: nstep, init
 integer, dimension(:), allocatable :: rb
-real(rp) :: conv_thr
+real(rp) :: conv_thr, soc_scale
 real(rp), dimension(:), allocatable :: ws, mixmag
 
 namelist /self/ ws_all, all_inequivalent, &
    mix_all, magnetic_mixing, mixmag_all, freeze, orbital_polarization, &
-   rigid_band, rb, nstep, init, &
-   conv_thr, ws, mixmag
+   rigid_band, rb, nstep, init, soc_scale, &
+   conv_thr, ws, mixmag, fix_soc
